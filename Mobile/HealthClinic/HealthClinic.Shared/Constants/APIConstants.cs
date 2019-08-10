ba49ab718aa64cec107edd0df99491c73557aaf3
abel <abel.wang@gmail.com>
2019-08-10 11:44:29 -0700

    added a build number

--- Mobile/HealthClinic/HealthClinic.Shared/Constants/APIConstants.cs
+++ Mobile/HealthClinic/HealthClinic.Shared/Constants/APIConstants.cs
@@ -5,5 +5,16 @@
         public const string GetFoodLogsUrl = "https://mercuryhealth-dev.azurewebsites.net/api/FoodLogApi";
         public const string PostFoodUrl = "https://abelmercuryhealthservice-dev.azurewebsites.net/ImageIDAPI/UploadFoodImage1";
         public const string DeleteFoodLogUrl = "https://abelmercuryhealthservice-dev.azurewebsites.net/FoodApi/DeleteFoodItem";
+
+        public static string buildNumber = "__Build_BuildNumber__";
+
+        public static string GetTheFoodLogsUrl
+        {
+
+            get
+            {
+                return "";
+            }
+        }
     }
 }
