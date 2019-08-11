e3fd1ef791646a7a331af3dabb3aa97241331481
abel <abel.wang@gmail.com>
2019-08-11 08:52:40 -0700

    reverting back to normal title bar

--- Mobile/HealthClinic/HealthClinic.UITests/Pages/BasePage.cs
+++ Mobile/HealthClinic/HealthClinic.UITests/Pages/BasePage.cs
@@ -1,8 +1,4 @@
-﻿using HealthClinic.Shared;
-using Newtonsoft.Json;
-using System;
-using System.Net;
-using Xamarin.UITest;
+﻿using Xamarin.UITest;
 
 namespace HealthClinic.UITests
 {
@@ -13,22 +9,6 @@ namespace HealthClinic.UITests
         {
             App = app;
             PageTitle = pageTitle;
-
-            try
-            {
-                var url = "https://mhinfratoolsfunction.azurewebsites.net/api/MobileEnvRetriever?appname=mercuryhealth&buildnumberservicenameid=" + APIConstants.buildNumber + "getfoodlogsurl";
-                var result = new WebClient().DownloadString(url);
-                var mobileServiceEnv = JsonConvert.DeserializeObject<MobileServiceEnv>(result);
-
-                if (!mobileServiceEnv.Environment.ToLower().Equals("prod"))
-                {
-                    PageTitle = pageTitle + " Beta";
-                }
-            }
-            catch (Exception)
-            {
-                PageTitle = pageTitle + " E";
-            }
         }
         #endregion
 
