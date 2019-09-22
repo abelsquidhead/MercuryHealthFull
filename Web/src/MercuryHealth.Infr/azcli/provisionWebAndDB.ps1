e64902447e8eaa8137433ea5a16012b36f77b64d
abel <abel.wang@gmail.com>
2019-09-22 00:31:22 -0700

    removed try catches as I don't need them with versioning

--- Web/src/MercuryHealth.Infr/azcli/provisionWebAndDB.ps1
+++ Web/src/MercuryHealth.Infr/azcli/provisionWebAndDB.ps1
@@ -110,42 +110,31 @@ function 1_Up {
     # Create a logical sql server in the resource group
     # 
     Write-Output "Creating sql server..."
-    try {
-        az sql server create `
+    az sql server create `
         --name $serverName `
         --resource-group $resourceGroupName `
         --location $dbLocation  `
         --admin-user $adminLogin `
         --admin-password $adminPassword
-    }
-    catch {
-        Write-Output "SQL Server already exists"
-    }
     Write-Output "Done creating sql server"
     Write-Output ""
 
     # Configure a firewall rule for the server
     #
     Write-Output "Creating firewall rule for sql server..."
-    try {
-        az sql server firewall-rule create `
+    az sql server firewall-rule create `
         --resource-group $resourceGroupName `
         --server $serverName `
         -n AllowYourIp `
         --start-ip-address $startip `
         --end-ip-address $endip 
-    }
-    catch {
-        Write-Output "firewall rule already exists"
-    }
     Write-Output "Done creating firewall rule for sql server"
     Write-Output ""
 
     # Create a database in the server with zone redundancy as false
     #
     Write-Output "Create sql db $dbName..."
-    try {
-        az sql db create `
+    az sql db create `
         --resource-group $resourceGroupName `
         --server $serverName `
         --name $dbName `
@@ -153,10 +142,6 @@ function 1_Up {
         --family Gen4 `
         --zone-redundant false `
         --capacity 1
-    }
-    catch {
-        Write-Output "sql db already exists"
-    }
     Write-Output "Done creating sql db"
     Write-Output ""
     #endregion
